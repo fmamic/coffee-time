4d7d1df2293732c27bf38e36210e5614fbfefa1a
efilmam <filip.mamic@ericsson.com>
2018-10-24 16:09:24 +0100

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const routes = require('./routes/users');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// set up express app
+const app = express();
+
+mongoose.connect('mongodb://localhost/coffee-time', { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+
+app.use(bodyParser.json());
+
+// initialize routes
+app.use('/api', routes);
+
+// error handling
+app.use(function(err, req, res, next) {
+    // console.log(err);
+    res.status(422).send({error: err.message});
+});
+
+// listen for requests
+app.listen(process.env.port || 4000, function() {
+    console.log('Now listening for requests');
+});
---
+++ server/models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create User schema & model
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Name field is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email field is required']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    available: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const User = mongoose.model('user', UserSchema);
+module.exports = User;
