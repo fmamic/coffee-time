a6f3eaf46111b47b145e55e2beefc9b870567dca
Filip Mamic <filipmamic@hotmail.com>
2018-10-27 18:29:06 +0100

    Setting up packaging and docker compose

--- api/server.js
+++ api/server.js
@@ -2,11 +2,20 @@ const express = require('express');
 const routes = require('./routes/users');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 // set up express app
 const app = express();
 
-mongoose.connect('mongodb://mongo:27017/coffee-time', { useNewUrlParser: true });
+if (process.env.NODE_ENV !== 'production') {
+    dotenv.load();
+}
+
+const mongoPort = process.env.MONGO_PORT;
+const mongoHostname = process.env.MONGO_HOSTNAME;
+const mongoDatabase = "coffee-time";
+
+mongoose.connect(`mongodb://${mongoHostname}:${mongoPort}/${mongoDatabase}`, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 app.use(bodyParser.json());
