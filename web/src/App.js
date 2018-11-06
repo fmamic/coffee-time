a2807efea9b34dcb3f2aaceb7d0d42815e1e74ad
Filip Mamic <filipmamic@hotmail.com>
2018-11-06 22:59:37 +0000

    Creating Login Form

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom'
 
-const NewRoute = () => {
-  return (
-    <div></div>
-  );
-}
-
 class App extends Component {
   render() {
     return (
---
+++ web/src/login/LoginForm.js
@@ -0,0 +1,24 @@
+import React, { PropTypes } from 'react';
+import TextInput from './common/TextInput';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import * as sessionActions from '../action/sessionActions';
+
+class LoginForm extends React.Component {
+
+    constructor(props) {
+
+    }
+
+    onChange(event) {
+
+    }
+
+    onSave(event) {
+
+    }
+
+    render() {
+        return(<div>TextInput</div>);
+    }
+}
